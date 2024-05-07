4676e7f4e262bbea234f88200af64284551acf1e
fanus abraha <fanus.abraha@yahoo.com>
2024-05-07 15:08:28 -0500

    Editing the html

--- exercise/src/main/java/trainingJS/exercise/controller/UserController.java
+++ exercise/src/main/java/trainingJS/exercise/controller/UserController.java
@@ -1,4 +1,16 @@
 package trainingJS.exercise.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import trainingJS.exercise.domain.User;
+
+@Controller
 public class UserController {
+    @GetMapping ("/login")
+    public String createLogin(ModelMap map){
+        User user= new User();
+        map.put("user",user);
+        return "login";
+    }
 }
