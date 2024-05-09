4bb74e70807fb3b5e58e873911470e6e28216f3a
fanus abraha <fanus.abraha@yahoo.com>
2024-05-09 12:26:13 -0500

    Editing the html

---
+++ exercise/src/main/resources/static/login.js
@@ -0,0 +1,18 @@
+ submitbtn.addEventListener( 'click', ()=>{
+        var username= document.querySelector("#username")
+        var password= document.querySelector("#password")
+        if (username.value=='' || password.value==''){ console.log(`please enter email and password`)}
+        else {console.log(`you submitted valid data`)}
+        })
+
+        submitbtn.addEventListener('click', submitBtnClick)
+        function submitBtnClick () {alert(`i have been clicked`)}
+        var submitButton = document.querySelector("#submitbtn")
+                console.log(`${submitButton}`)
+
+        submitButton.addEventListener('focus', function(){console.log(`focused on the inputs`)})
+
+        document.getElementById("submitbtn").addEventListener("mouseover",
+                                            function(){this.style.backgroundColor="blue"})
+        document.getElementById("submitbtn").addEventListener("mouseout",
+                                            function(){this.style.backgroundColor="green"})
